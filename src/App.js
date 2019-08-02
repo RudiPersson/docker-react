b595edbec754e07e1f4fcf2dccad1a18a22bf6cc
Rudi Persson <rpe@ft.fo>
2019-08-02 10:34:10 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          Hello Docker
+          Changed on the feature branch{" "}
         </p>
         <a
           className="App-link"
